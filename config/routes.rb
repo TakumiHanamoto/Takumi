1a23475de1b553f873af0216ef5599163b464755
TakumiHanamoto <Takumossan4151@outlook.jp>
2021-08-27 12:05:48 +0900

    Debug2

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+  root 'hello#index'
+  get 'hello/index' => 'hello#index'
+  get 'hello/link' => 'hello#link'
   get 'students/index'
   get 'students/show'
   get 'requesters/index'
   get 'requesters/show'
   devise_for :students
-  resources :students, only: [:show]
+  resources :students, only: [:index, :show]
   devise_for :requesters
-  resources :requesters, only: [:show]
-  root 'hello#index'
-  get 'hello/index' => 'hello#index'
-  get 'hello/link' => 'hello#link'
+  resources :requesters, only: [:index, :show]
+  
 
   resources :orders do
     resources :likes, only: [:create, :destroy]
